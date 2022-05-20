6b3038d3f97be1e708fca9bd4d4126a0b868a8b2
Eric Wang <eric.wang401@gmail.com>
2022-05-21 00:49:18 +0200

    Fix vite

--- resources/scripts/App.tsx
+++ resources/scripts/App.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-    return <h1>test</h1>
+    return <h1>tesfsfsftf</h1>
 }
 
 export default App
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,14 +24,15 @@
         @env('local')
             @verbatim
                 <script type="module" src="http://localhost:3000/@vite/client"></script>
+
+                <script type="module">
+                    import RefreshRuntime from "http://localhost:3000/@react-refresh"
+                    RefreshRuntime.injectIntoGlobalHook(window)
+                    window.$RefreshReg$ = () => {}
+                    window.$RefreshSig$ = () => (type) => type
+                    window.__vite_plugin_react_preamble_installed__ = true
+                </script>
             @endverbatim
-            <script type="module">
-                import RefreshRuntime from "http://localhost:3000/@react-refresh"
-                RefreshRuntime.injectIntoGlobalHook(window)
-                window.$RefreshReg$ = () => {}
-                window.$RefreshSig$ = () => (type) => type
-                window.__vite_plugin_react_preamble_installed__ = true
-            </script>
         @endenv
     </head>
     <body>
