7cfa827a05e1eeef4f51ced79a6f9e649bc268e6
Eric Wang <eric.wang401@gmail.com>
2022-05-19 22:19:54 -0500

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-import laravel from 'vite-plugin-laravel'
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import laravel from "vite-plugin-laravel";
 
 export default defineConfig({
-	plugins: [
-		laravel({
-			postcss: [
-				tailwindcss(),
-				autoprefixer(),
-			],
-		}),
-	],
-})
+    server: {
+        host: "0.0.0.0",
+    },
+    plugins: [
+        laravel({
+            postcss: [tailwindcss(), autoprefixer()],
+        }),
+    ],
+});
